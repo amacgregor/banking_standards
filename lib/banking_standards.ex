6999efc5c5d6ff840ba469aaa533d929d288bb9b
Allan MacGregor <amacgregor@allanmacgregor.com>
2024-12-23 09:24:00 -0400

    Clean scaffold

---
+++ lib/banking_standards.ex
@@ -0,0 +1,18 @@
+defmodule BankingStandards do
+  @moduledoc """
+  Documentation for `BankingStandards`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BankingStandards.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/banking_standards_test.exs
@@ -0,0 +1,8 @@
+defmodule BankingStandardsTest do
+  use ExUnit.Case
+  doctest BankingStandards
+
+  test "greets the world" do
+    assert BankingStandards.hello() == :world
+  end
+end
